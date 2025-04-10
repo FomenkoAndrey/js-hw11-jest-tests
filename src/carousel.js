e0725f59cd8910ac8daae1583ebb20cdbbdf9c09
Alecto <fomenkoandrey1978@gmail.com>
2025-04-10 11:05:36 +0300

    Refactor(carousel): remove unused _initConfig method and clean up code

--- src/carousel.js
+++ src/carousel.js
@@ -11,58 +11,12 @@
 class Carousel {
   constructor(p) {
     const settings = {...{containerId: '#carousel', interval: 5000, isPlaying: true, slideId: '.slide'}, ...p};
-    // const settings = this._initConfig(params);
 
     this.container = document.querySelector(settings.containerId);
     this.slideItems = this.container.querySelectorAll(settings.slideId);
     this.TIMER_INTERVAL = settings.interval;
     this.isPlaying  = settings.isPlaying;
   }
-
-  /*
-   *
-   *_initConfig(objectWithInnerParams) {
-   *
-   * Вариант 1
-   *
-   * const defaultSettings = {
-   *   containerID: '#carousel',
-   *   interval: 5000,
-   *   slideID: '.slide',
-   *   isPlaying: true
-   * };
-   *
-   * const resultObject = {};
-   *
-   * if (objectWithInitParams === undefined) {
-   *   return defaultSetting;
-   * }
-   *
-   * resultObject.containerID = objectWithInnerParams.containerID || defaultSetting.containerID;
-   * resultObject.slideID = objectWithInnerParams.slideID || defaultSetting.slideID;
-   * resultObject.interval = objectWithInnerParams.interval || defaultSetting.interval;
-   * resultObject.isPlaying = objectWithInnerParams.isPlaying || defaultSetting.isPlaying;
-   *
-   * return resultObject;
-   *
-   *
-   * Вариант 2
-   *
-   * const p = {containerID: '#carousel', slideID: '.slide', interval: 5000, isPlaying: true};
-   *
-   * return {...p, ...o};
-   *
-   *
-   * Вариант 3
-   *
-   * return {...{containerID: '#carousel', interval: 5000, slideID: '.slide'}, ...o};
-   *
-   * }
-   *
-   * Вариант 4 - переписать метод через стрелочную функцию (ругается линтер, но работает)
-   * _initConfig = (o) => ({...{containerID: '#carousel', interval: 5000, slideID: '.slide'}, ...o})
-   *
-   */
 
   /* private, _initProps - initialization properties */
   _initProps() {
